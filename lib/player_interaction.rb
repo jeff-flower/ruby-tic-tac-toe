a8842076f6ff2b5f45daefcc2a06be97a6b901fa
Jeff Flower <jeff.flower14@gmail.com>
2025-02-25 16:05:08 -0700

    Failing test for command line interaction

---
+++ lib/player_interaction.rb
@@ -0,0 +1,5 @@
+# accepts board
+# player interaction prompts for input on command line
+# translates input to move on board
+# DOES NOT: check for win
+# DOES NOT: loop through players
---
+++ spec/player_interaction_spec.rb
@@ -0,0 +1,15 @@
+require './lib/board'
+require './lib/player_interaction'
+
+describe PlayerInteraction do
+  it 'prompts the given player for a move' do
+    # FILL ME IN!!
+    # board = Board.new(3)
+    # player_interaction = PlayerInteraction.new(board)
+    # player_interaction.get_player_move(1)
+    # expect stdout to eq "Player 1 move (row, column)"
+    expect(2 + 2).to be 4
+  end
+
+  # print game board somewhere
+end
