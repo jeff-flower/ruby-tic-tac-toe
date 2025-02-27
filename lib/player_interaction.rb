03dc190ac993ad72909227571e61d3c889bdb525
Jeff Flower <jeff.flower14@gmail.com>
2025-02-27 14:39:49 -0700

    Writes to command line with passing tests

--- lib/player_interaction.rb
+++ lib/player_interaction.rb
@@ -1,5 +1,20 @@
-# accepts board
 # player interaction prompts for input on command line
 # translates input to move on board
 # DOES NOT: check for win
 # DOES NOT: loop through players
+class PlayerInteraction
+  def initialize(board, input = $stdin, output = $stdout)
+    @board = board
+    @input = input
+    @output = output
+  end
+
+  def get_player_move(player_number)
+    @output.puts "Player #{player_number} move (row, column)"
+    # row, column = @input.gets.chomp.split(',')
+
+    # @board.move(player_number, row, column)
+
+    # @output.puts "Player #{player_number} moved to #{row}, #{column}"
+  end
+end
--- spec/player_interaction_spec.rb
+++ spec/player_interaction_spec.rb
@@ -3,13 +3,22 @@ require './lib/player_interaction'
 
 describe PlayerInteraction do
   it 'prompts the given player for a move' do
-    # FILL ME IN!!
-    # board = Board.new(3)
-    # player_interaction = PlayerInteraction.new(board)
-    # player_interaction.get_player_move(1)
-    # expect stdout to eq "Player 1 move (row, column)"
-    expect(2 + 2).to be 4
+    board = Board.new(3)
+
+    input = StringIO.new("1,1\n")
+    output = StringIO.new
+
+    player_interaction = described_class.new(board, input, output)
+    player_interaction.get_player_move(1)
+    expect(output.string).to eq "Player 1 move (row, column)\n"
   end
 
-  # print game board somewhere
+  #xit 'parses a move from the command line' do
+    # board = Board.new(3)
+    # player_interaction = described_class.new(board)
+
+    # expect(board.board[0][0]).to be 1
+  #end
+
+  # print game board somewhere?
 end
